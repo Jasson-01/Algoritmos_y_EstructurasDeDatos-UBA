fa2e75391f62d5c35a681b4924cacc008e25ddd2
Jasson-01 <jason.davila001@gmail.com>
2025-12-10 23:34:41 -0300

    Taller2-Clase-Fecha
    Terminado

--- Taller-Laboratorio/Practicando/2Taller-POO/taller2/src/main/java/aed/Fecha.java
+++ Taller-Laboratorio/Practicando/2Taller-POO/taller2/src/main/java/aed/Fecha.java
@@ -1,37 +1,71 @@
 package aed;
 
 public class Fecha {
+    private int dia;
+    private int mes;
+
     public Fecha(int dia, int mes) {
         // Implementar
+        this.dia = dia;
+        this.mes = mes;
     }
 
     public Fecha(Fecha fecha) {
         // Implementar
+        this.dia = fecha.dia;
+        this.mes = fecha.mes;
     }
 
     public Integer dia() {
         // Implementar
-        return -1;
+        return this.dia;
     }
 
     public Integer mes() {
         // Implementar
-        return -1;
+        return this.mes;
     }
 
     public String toString() {
         // Implementar
-        return "";
+        return this.dia + "/" + this.mes;
     }
 
     @Override
     public boolean equals(Object otra) {
         // Implementar
-        return true;
+        if (otra == null){
+            return false;
+        }
+
+        boolean claseDistinta = otra.getClass() != this.getClass();
+        if(claseDistinta){
+            return false;
+        }
+        // casting -> cambiar el tipo
+        Fecha otraFecha = (Fecha) otra;
+        //                ↑↑↑↑↑↑↑
+        //                "Convierte 'otra' de Object a Fecha"
+
+        return this.dia == otraFecha.dia && this.mes == otraFecha.mes;
+        // this.dia == otraFecha.dia
+        //   ↑           ↑
+        //   |           └─ El día del objeto que recibimos como parámetro
+        //   └──────────── El día del objeto actual (this)
+
     }
 
     public void incrementarDia() {
         // Implementar
+        this.dia++;
+        if(this.dia > diasEnMes(this.mes)){
+           this.dia = 1;
+           if( this.mes == 12){
+              this.mes = 1;
+           } else {
+              this.mes += 1;
+           }
+        }
     }
 
     private int diasEnMes(int mes) {
