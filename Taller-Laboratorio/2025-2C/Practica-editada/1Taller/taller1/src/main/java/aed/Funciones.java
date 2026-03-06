50f0e23fa4d01354318b89f25e6c97891c4e6541
Jasson-01 <jason.davila001@gmail.com>
2026-03-06 18:01:17 -0300

    Practicando 1tp - java
    AED

--- Taller-Laboratorio/2025-2C/Practica-editada/1Taller/taller1/src/main/java/aed/Funciones.java
+++ Taller-Laboratorio/2025-2C/Practica-editada/1Taller/taller1/src/main/java/aed/Funciones.java
@@ -2,85 +2,149 @@ package aed;
 
 class Funciones {
 
-/***  Primera parte: Funciones en java ***/
+    /*** Primera parte: Funciones en java ***/
 
     int cuadrado(int x) {
         // COMPLETAR
-        return 0;
+        return x * x;
     }
 
     double distancia(double x, double y) {
         // COMPLETAR
-        return 0.0;
+        double op = x * x + y * y;
+        return Math.sqrt(op);
     }
 
     boolean esPar(int n) {
         // COMPLETAR
-        return false;
+        return n % 2 == 0;
     }
 
     boolean esBisiesto(int n) {
         // COMPLETAR
+        if ((n % 4 == 0 && n % 100 != 0) || n % 400 == 0) {
+            return true;
+        }
         return false;
     }
 
     int factorialIterativo(int n) {
         // COMPLETAR
-        return 0;
+        int res = 1;
+        for (int i = n; i > 0; i--) {
+            res *= i;
+        }
+        return res;
     }
 
     int factorialRecursivo(int n) {
         // COMPLETAR
-        return 0;
+        int res = 1;
+        if (n == 0) {
+            res = 1;
+        } else {
+            res = n * factorialRecursivo(n - 1);
+        }
+        return res;
     }
 
     boolean esPrimo(int n) {
         // COMPLETAR
-        return false;
+        if (n == 2) {
+            return true;
+        } else if (n < 2) {
+            return false;
+        }
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     int sumatoria(int[] numeros) {
         // COMPLETAR
-        return 0;
+        int acumulador = 0;
+        for (int i = 0; i < numeros.length; i++) {
+            acumulador += numeros[i];
+        }
+        return acumulador;
     }
 
     int busqueda(int[] numeros, int buscado) {
         // COMPLETAR
-        return 0;
+        int pos = 0;
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] == buscado) {
+                pos = i;
+            }
+        }
+        return pos;
     }
 
     boolean tienePrimo(int[] numeros) {
         // COMPLETAR
+        for (int i = 0; i < numeros.length; i++) {
+            if (esPrimo(numeros[i])) {
+                return true;
+            }
+        }
         return false;
     }
 
     boolean todosPares(int[] numeros) {
         // COMPLETAR
-        return false;
+        for (int i = 0; i < numeros.length; i++) {
+            if (!esPar(numeros[i])) {
+                return false;
+            }
+        }
+        return true;
     }
 
     boolean esPrefijo(String s1, String s2) {
         // COMPLETAR
-        return false;
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+        for (int i = 0; i < s1.length(); i++) {
+            if (s2.charAt(i) != s1.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     boolean esSufijo(String s1, String s2) {
         // COMPLETAR
-        return false;
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+        int longitudS2 = s2.length() - 1;
+        for (int i = s1.length() - 1; i >= 0; i--) {
+            if (s2.charAt(longitudS2) != s1.charAt(i)) {
+                return false;
+            }
+            longitudS2--;
+        }
+        return true;
     }
 
-/***  Segunda parte: Debugging ***/
+    /*** Segunda parte: Debugging ***/
 
     boolean xor(boolean a, boolean b) {
-        return a || b && !(a && b);
+        return (a || b) && (!(a && b));
     }
 
     boolean iguales(int[] xs, int[] ys) {
         boolean res = true;
-
-        for (int i = 0; i < xs.length; i++) {
-            if (xs[i] != ys[i]) {
+        int j = 0;
+        for (int i = 0; i <= xs.length - 1; i++) {
+            if ((xs[i] != ys[j]) || (xs.length != ys.length)) {
                 res = false;
+            } else {
+                j++;
             }
         }
         return res;
@@ -88,8 +152,8 @@ class Funciones {
 
     boolean ordenado(int[] xs) {
         boolean res = true;
-        for (int i = 0; i < xs.length; i++) {
-            if (xs[i] > xs [i+1]) {
+        for (int i = 0; i < xs.length - 1; i++) {
+            if (xs[i] > xs[i + 1]) {
                 res = false;
             }
         }
@@ -97,20 +161,21 @@ class Funciones {
     }
 
     int maximo(int[] xs) {
-        int res = 0;
-        for (int i = 0; i <= xs.length; i++) {
-            if (xs[i] > res) res = i;
+        int res = xs[0]; // ojo
+        for (int i = 0; i <= xs.length - 1; i++) {
+            if (xs[i] > res)
+                res = xs[i]; // ojo
         }
         return res;
     }
 
     boolean todosPositivos(int[] xs) {
-        boolean res = false;
+        boolean res = true; // OJO
         for (int x : xs) {
             if (x > 0) {
                 res = true;
             } else {
-                res = false;
+                return false; // OJO
             }
         }
         return res;
