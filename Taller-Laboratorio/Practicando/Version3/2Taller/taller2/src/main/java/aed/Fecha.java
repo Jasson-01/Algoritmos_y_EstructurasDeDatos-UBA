ad6d31685ac8c66a3c55c536b25e8f7ac2028252
jasson-01 <jason.davila001@gmail.com>
2026-06-23 12:02:27 -0300

    Tarea-Fecha-Terminado :D

--- Taller-Laboratorio/Practicando/Version3/2Taller/taller2/src/main/java/aed/Fecha.java
+++ Taller-Laboratorio/Practicando/Version3/2Taller/taller2/src/main/java/aed/Fecha.java
@@ -1,38 +1,75 @@
 package aed;
 
 public class Fecha {
+    private int dia;
+    private int mes;
+
     public Fecha(int dia, int mes) {
         // Implementar
+        this.dia = dia;
+        this.mes = mes;
     }
 
     public Fecha(Fecha fecha) {
         // Implementar
+        dia = fecha.dia;
+        mes = fecha.mes;
     }
 
     public Integer dia() {
         // Implementar
-        return -1;
+        return dia;
     }
 
     public Integer mes() {
         // Implementar
-        return -1;
+        return mes;
     }
 
     public String toString() {
         // Implementar
-        return "";
+        return dia() + "/" + mes();
     }
 
     @Override
     public boolean equals(Object otra) {
         // Implementar
-        return true;
+        
+        // 2. Si el otro objeto es nulo(otraEsNull) o no es de la clase Fecha(claseDistinta), no son iguales
+        boolean otraEsNull = (otra == null);
+        if(otraEsNull){
+            return false;
+        }
+
+        boolean claseDistinta = otra.getClass() != this.getClass();
+        if(claseDistinta){
+            return false;
+        }
+        
+        // cambio el tipo de dato
+        Fecha otraFecha = (Fecha) otra;
+        
+         // 4. Comparamos los atributos clave
+        return dia == otraFecha.dia && mes == otraFecha.mes;
     }
 
     public void incrementarDia() {
         // Implementar
+        //  System.out.println(diasEnMes(6)); --> res = 30
+        if ( mes > 12) {
+           mes = 1;
+           dia = 1;
+        } else {
+            if ( (diasEnMes(mes) == 28 && mes == 2) || (diasEnMes(mes) == 30 && dia == 30) || (diasEnMes(mes) == 31 && dia == 31) ){
+                dia = 1;
+                mes += 1;
+            } else {
+               dia += 1;
+            }
+        }
+
     }
+        // dia -> 28 mes -> 6
 
     private int diasEnMes(int mes) {
         int dias[] = {
@@ -41,6 +78,7 @@ public class Fecha {
                 // jul, ago, sep, oct, nov, dic
                 31, 31, 30, 31, 30, 31
         };
+        // OJO, a mes se le resta 1 por eso el array comienza con 1 no con 0.
         return dias[mes - 1];
     }
 
