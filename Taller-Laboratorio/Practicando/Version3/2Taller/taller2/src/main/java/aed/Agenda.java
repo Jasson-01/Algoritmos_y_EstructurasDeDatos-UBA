0d7ef7edf360bf491e44475b007b0e5845b2bb39
jasson-01 <jason.davila001@gmail.com>
2026-06-27 11:33:00 -0300

    Agenda-terminado-taller 2-terminado

--- Taller-Laboratorio/Practicando/Version3/2Taller/taller2/src/main/java/aed/Agenda.java
+++ Taller-Laboratorio/Practicando/Version3/2Taller/taller2/src/main/java/aed/Agenda.java
@@ -1,28 +1,42 @@
 package aed;
 
 public class Agenda {
+    private Fecha fechaActual;
+    private ArregloRedimensionableDeRecordatorios conjRecordatorios;
 
-    public Agenda(Fecha fechaActual) {
+    public Agenda(Fecha fechaActual) { // Constructor por copia
         // Implementar
+        this.fechaActual = new Fecha(fechaActual);
+        this.conjRecordatorios = new ArregloRedimensionableDeRecordatorios();
     }
 
     public void agregarRecordatorio(Recordatorio recordatorio) {
         // Implementar
+        conjRecordatorios.agregarAtras(recordatorio);
     }
 
     @Override
     public String toString() {
         // Implementar
-        return "";
+        String toDo = "";
+        for (int i = 0; i < conjRecordatorios.longitud(); i++) {
+            Recordatorio recordatorio = conjRecordatorios.obtener(i);
+            if (recordatorio.fecha().equals(this.fechaActual)) {
+                toDo += recordatorio.toString() + "\n";
+            }
+        }
+
+        return fechaActual.dia() + "/" + fechaActual.mes() + "\n" + "=====" + "\n" + toDo;
     }
 
     public void incrementarDia() {
         // Implementar
+        fechaActual.incrementarDia();
     }
 
     public Fecha fechaActual() {
         // Implementar
-        return null;
+        return new Fecha(fechaActual);
     }
 
 }
